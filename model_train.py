094d3061b7df0d3727410d2215312fee0e1212c9
cshuihui <xzj200669@foxmail.com>
2026-05-09 10:00:09 +0800

    11

--- model_train.py
+++ model_train.py
@@ -4,18 +4,18 @@ model = YOLO('yolo11n-pose.pt')
 
 model.train(data='mymodel-yolo11-pose.yaml',
             workers=0,
-            epochs=500,
+            epochs=200,
             batch=32,
             lr0=0.0001,
+            lrf=0.01,
+            optimizer='AdamW',
+
             pose=40,
-            box=5,
+            box=15,
 
 
             scale=0.3,
             translate=0.1,
-
-
-
             mosaic=0,
             mixup=0,
             copy_paste=0
